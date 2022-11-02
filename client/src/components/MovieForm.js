692ba0915134faa13dd2fae27dd42bdfb95ee47e
Zachary Youngdale <zyoungdale@gmail.com>
2022-11-02 15:29:26 -0400

    commit tree 4db55ea5865f4b671a4b03028e5063495d25f414

--- client/src/components/MovieForm.js
+++ client/src/components/MovieForm.js
@@ -13,18 +13,41 @@ function MovieForm() {
     discount: false,
     female_director: false,
   });
+  const [errors, setErrors] = useState([]);
 
-  function handleSubmit(e) {
+  // function handleSubmit(e) {
+  //   e.preventDefault();
+  //   fetch("/movies", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify(formData),
+  //   }).then((response) => {
+  //     if (response.ok) {
+  //       response.json().then((newMovie) => console.log(newMovie));
+  //     } else {
+  //       response.json().then((errorData) => setErrors(errorData.errors));
+  //     }
+  //   });
+  // }
+
+  async function handleSubmit(e) {
     e.preventDefault();
-    fetch("/movies", {
+    const response = await fetch("/movies", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(formData),
-    })
-      .then((response) => response.json())
-      .then((newMovie) => console.log(newMovie));
+    });
+
+    const data = await response.json();
+    if (response.ok) {
+      console.log("Movie created:", data);
+    } else {
+      setErrors(data.errors);
+    }
   }
 
   function handleChange(e) {
@@ -125,6 +148,13 @@ function MovieForm() {
             />
           </label>
         </FormGroup>
+        {errors.length > 0 && (
+          <ul style={{ color: "red" }}>
+            {errors.map((error) => (
+              <li key={error}>{error}</li>
+            ))}
+          </ul>
+        )}
         <SubmitButton type="submit">Add Movie</SubmitButton>
       </form>
     </Wrapper>
